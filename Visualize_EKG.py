97e5e9fdfb38ca2d8bd08209557a4b1213be6778
Matthew Lemoine <mlemoine559@gmail.com>
2026-02-12 11:04:20 -0600

    stuff

--- Visualize_EKG.py
+++ Visualize_EKG.py
@@ -6,10 +6,15 @@ import pandas as pd
 import matplotlib as plt
 import wfdb
 
-def make_EKG_plot(path_to_file): #The input here, is the directions to the .hea and .dat files. (should be a string)
-    
-    record = wfdb.rdrecord(path_to_file)
 
-    wfdb.plot_wfdb(record =record, title='Patient{recrod}', figsize=(14,8), ecg_grids = 'all', sharex = True)
+record = wfdb.rdrecord("Brugada_dataset/files/188981/188981")
+
+wfdb.plot_wfdb(record =record, title='Patient{recrod}', figsize=(14,8), ecg_grids = 'all', sharex = True)
+
+# def make_EKG_plot(path_to_file): #The input here, is the directions to the .hea and .dat files. (should be a string)
     
-# You can get a list of all the patients in the 'files' file and then have a while loop cycle through that list. (this is for main_ekg.py)
+#     record = wfdb.rdrecord(path_to_file)
+
+#     wfdb.plot_wfdb(record =record, title='Patient{recrod}', figsize=(14,8), ecg_grids = 'all', sharex = True)
+    
+# # You can get a list of all the patients in the 'files' file and then have a while loop cycle through that list. (this is for main_ekg.py)
