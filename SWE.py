4b67a449edf30909e8a41154fd63f210ea8f7cdb
Matthew Lemoine <mlemoine559@gmail.com>
2026-02-20 10:39:09 -0600

    for some reason SWE.py wasn't included

---
+++ SWE.py
@@ -0,0 +1,115 @@
+# This file is for the SWE piece of things.
+
+import numpy as np
+import matplotlib as plt
+import Visualize_EKG as vekg
+import wfdb
+import matplotlib.pyplot as pyplt
+import Wavelet_approx as wa
+
+"""
+Note the first 2 functions are strictly for plotting.
+The next 3 functions will fix the sample of points taken to be the length of the signal. (for a signal of about 1000 points it will take about 100 seconds.)
+There is a function below them, that doesn't have a fixed sample points taken place. Use this to find a suitable tau.
+"""
+
+def plot_3d(projected_points):
+
+    fig = pyplt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    for x,y,z in projected_points:
+        ax.scatter(x,y,z)
+
+    return pyplt.show()
+
+def plot_2d(projected_points):
+
+    fig = pyplt.figure()
+    ax = fig.add_subplot(projection='2d')
+
+    for x,y in projected_points:
+        ax.scatter(x,y)
+
+    return pyplt.show()
+
+"""
+The following function will retrieve the desired points in whatever dimensional projection space you want.
+M tells you which dimension to push to. (e.g. M = 2  ==> R^3)
+tau tells you about what's happening in the periodicity of your signal. 
+"""
+
+def SWE_get_points_nd(ptf, lead_s, wavelet, level_decomp, tau, M):
+
+    signal = vekg.get_EKG_leads(ptf, lead_s)
+
+    projected_points = []
+    points_in_interval = np.linspace(0, len(signal), len(signal))
+
+    i = 0
+    while i < len(signal):
+        p = points_in_interval[i]
+        point = []
+        j = 0
+        while j <= M: # if t > 1200 k = 0; else: k = blah blah
+            if p+j*tau > len(signal):
+                k = 0
+            else:
+                k = wa.wavelet(ptf, lead_s, wavelet, level_decomp, p + j*tau)
+            point.append(k)
+            j = j+1
+        projected_points.append(point)
+        i = i+1
+    
+    return projected_points
+
+"""
+The following code will return a 3d plot of the projected wavelet. Here we fix M = 2.
+"""
+def SWE_plot_3d(ptf, lead_s, wavelet, level_decomp, tau, naming_things):
+    M = 2
+
+    projected_points = SWE_get_points_nd(ptf, lead_s, wavelet, level_decomp, tau, M)
+    
+    return plot_3d(projected_points)
+
+"""
+The following code will return a 3d plot of the projected wavelet. Here we fix M = 1.
+"""
+def SWE_plot_2d(ptf, lead_s, wavelet, level_decomp, tau, naming_things):
+    M = 1
+
+    projected_points = SWE_get_points_nd(ptf, lead_s, wavelet, level_decomp, tau, M)
+    
+    return plot_2d(projected_points)
+
+
+"""
+The following function is to test and find a suitable tau.
+After this function, you may want to plot them, if your M = 1,2.
+Use the plot_2d or plot_3d respectively.
+"""
+
+def SWE_get_points_nd_TESTING(ptf, lead_s, wavelet, level_decomp, tau, M, sample_points):
+
+    signal = vekg.get_EKG_leads(ptf, lead_s)
+
+    projected_points = []
+    points_in_interval = np.linspace(0, len(signal), len(signal))
+
+    i = 0
+    while i < sample_points:
+        p = points_in_interval[i]
+        point = []
+        j = 0
+        while j <= M: # if t > 1200 k = 0; else: k = blah blah
+            if p+j*tau > len(signal):
+                k = 0
+            else:
+                k = wa.wavelet(ptf, lead_s, wavelet, level_decomp, p + j*tau)
+            point.append(k)
+            j = j+1
+        projected_points.append(point)
+        i = i+1
+
+    return projected_points
