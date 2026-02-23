063c320c684ac10b3d191387acec11853e5605e5
Matthew Lemoine <mlemoine559@gmail.com>
2026-02-23 17:34:54 -0600

    little edits

--- Normalize_EKG.py
+++ Normalize_EKG.py
@@ -14,3 +14,8 @@ import Visualize_EKG as vekg
 # You will want an input to be the normalization frequency.
 # Big question: How do you stretch out or shrink down a signal to fit in xx bpm?
 
+def find_peaks(ptf, lead_s)
+
+def trim_EKG(ptf, lead_s, num_peaks)
+
+def normalize(ptf, lead_s, bpm)
